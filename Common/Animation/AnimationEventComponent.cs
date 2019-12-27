92b3dedc296e946903c0c24b4d3c9e185ce9c896
3094997376@qq.com <3094997376@qq.com>
2019-12-27 17:02:27 +0800

    动画帧事件监听

--- Common/Animation/AnimationEventComponent.cs
+++ Common/Animation/AnimationEventComponent.cs
@@ -2,7 +2,28 @@
 
 public class AnimationEventComponent : MonoBehaviour
 {
+    Animation ani;
+    string clipName;
+    private void Start()
+    {
+        ani = GetComponent<Animation>();
+    }
+    /// <summary>
+    /// 播放某一个动画
+    /// </summary>
+    public void PlayForward(string _clipName,int offset=0,float speed=1)
+    {
+        clipName = _clipName;
+        //速率
+        AnimationState state=ani[clipName];
+        state.speed = 1;
 
+        //偏移量
+        AnimationClip clip = ani.GetClip(clipName);
+        state.normalizedTime = offset / clip.frameRate;
+
+        ani.Play(clipName);   
+    }
     /// <summary>
     /// 添加帧事件
     /// </summary>
@@ -14,7 +35,7 @@ public class AnimationEventComponent : MonoBehaviour
         ParaObj para = ScriptableObject.CreateInstance<ParaObj>();
         para.action = action;
 
-        Animation ani = GetComponent<Animation>();
+        //Animation ani = GetComponent<Animation>();
         AnimationClip clip = ani.GetClip(clipName);
 
         AnimationEvent ae = new AnimationEvent();
@@ -24,10 +45,65 @@ public class AnimationEventComponent : MonoBehaviour
         clip.AddEvent(ae);
     }
 
+
     void Callback(Object obj)
     {
         // Debug.Log("animation event call  " + obj.GetType() + "   " + obj.name);
         (obj as ParaObj).action();
         (obj as ParaObj).action = null;
     }
+
+    /// <summary>
+    /// 移除某一帧某个事件
+    /// </summary>
+    /// <param name="clipName"></param>
+    /// <param name="frame"></param>
+    public void ClearSpecialEvents(string clipName,int frame)
+    {
+        //Animation ani = GetComponent<Animation>();
+        AnimationClip clip = ani.GetClip(clipName);
+        float time = frame / ani.clip.frameRate;
+        for (int i = 0; i <clip.events.Length; i++)
+        {
+            if (clip.events[i].time==time)
+            {
+                clip.events.SetValue(null, i);
+                break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 移除某一个动画的所有帧事件
+    /// </summary>
+    /// <param name="clipName"></param>
+    /// <param name="frame"></param>
+    public void ClearAllEvents(string clipName, int frame)
+    {
+        //Animation ani = GetComponent<Animation>();
+        AnimationClip clip = ani.GetClip(clipName);
+        clip.events = null;
+    }
+
+
+    /// <summary>
+    /// 停止播放
+    /// </summary>
+    /// <param name="clipName"></param>
+    public void OnPause(string clipName)
+    {
+        //Animation ani = GetComponent<Animation>();            
+        ani[clipName].speed = 0;
+    }
+
+    /// <summary>
+    /// /继续播放
+    /// </summary>
+    /// <param name="clipName"></param>
+    public void OnContinue(string clipName)
+    {
+        Animation ani = GetComponent<Animation>();
+        ani[clipName].speed = 1;
+    }
 }
+
--- Common/Animation/AnimatorEventComponent.cs
+++ Common/Animation/AnimatorEventComponent.cs
@@ -1,8 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class AnimatorEventComponent : MonoBehaviour
 {
-
+    Animator ani;
+    string clipName;
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+    /// <summary>
+    /// 播放某一个动画
+    /// </summary>
+    public void PlayForward(string _clipName, int offset = 0, float speed = 1)
+    {
+        ani.speed = 1;
+        clipName = _clipName;
+        float timeLength = 0;
+        float timePoint = 0;
+        //速率
+        System.Array.FindIndex(ani.runtimeAnimatorController.animationClips, (ac) =>
+        {
+            if (ac.name == clipName)
+            {
+                timeLength = ac.length;
+                timePoint= offset/ac.frameRate;
+                return true;
+            }
+            return false;
+        });        
+        ani.Play(clipName,0,timePoint);             
+    }
     /// <summary>
     /// 添加帧事件(从所有动画片段层级中找)
     /// </summary>
@@ -11,6 +40,13 @@ public class AnimatorEventComponent : MonoBehaviour
     /// <param name="action">回调</param>
     public void AddEvent(string clipName, int frame, System.Action action)
     {
+        bool hasAdd = HasAddEvent(clipName, frame);
+        if (hasAdd)
+        {
+            return;
+        }
+
+
         ParaObj para = ScriptableObject.CreateInstance<ParaObj>();
         para.action = action;
 
@@ -33,6 +69,21 @@ public class AnimatorEventComponent : MonoBehaviour
         ac.AddEvent(ae);
     }
 
+
+     AnimationClip GetClip(string clip)
+    {
+        AnimationClip[] clips = ani.runtimeAnimatorController.animationClips;
+        AnimationClip ac = null;
+        for (int i = 0; i < clips.Length; i++)
+        {
+            if (clips[i].name == clip)
+            {
+                ac = clips[i];
+                break;
+            }
+        }
+        return ac;
+    }
     /// <summary>
     /// 添加帧事件(从某一层动画片段找)
     /// </summary>
@@ -41,21 +92,16 @@ public class AnimatorEventComponent : MonoBehaviour
     /// <param name="action">回调</param>
     public void AddEvent(int layer, string clipName, int frame, System.Action action)
     {
+        bool hasAdd= HasAddEvent(clipName, frame);
+        if (hasAdd)
+        {
+            return;
+        }
+
         ParaObj para = ScriptableObject.CreateInstance<ParaObj>();
         para.action = action;
 
-        Animator ani = GetComponent<Animator>();
-        AnimatorClipInfo[] clipInfors = ani.GetCurrentAnimatorClipInfo(layer);
-        AnimationClip ac = null;
-        for (int i = 0; i < clipInfors.Length; i++)
-        {
-            if (clipInfors[i].clip.name == clipName)
-            {
-                ac = clipInfors[i].clip;
-                break;
-            }
-        }
-
+        AnimationClip ac= GetClip(clipName);
         AnimationEvent ae = new AnimationEvent();
         ae.time = frame / ac.frameRate;
         ae.functionName = "Callback";
@@ -67,6 +113,87 @@ public class AnimatorEventComponent : MonoBehaviour
     {
         // Debug.Log("animation event call  " + obj.GetType() + "   " + obj.name);
         (obj as ParaObj).action();
-        (obj as ParaObj).action = null;
+        //(obj as ParaObj).action = null;
+    }
+
+    /// <summary>
+    /// 判断是否某一帧已经有帧事件,只添加一个
+    /// </summary>
+    /// <returns></returns>
+    bool HasAddEvent(string clipName,int frame)
+    {
+        bool hasAdd = false;
+        AnimationClip clip = GetClip(clipName);
+        float time = frame / clip.frameRate;
+        for (int i = 0; i < clip.events.Length; i++)
+        {
+            if (clip.events[i].time == time)
+            {
+                hasAdd = true;
+                break;
+            }
+        }
+        return hasAdd;
+    }
+    /// <summary>
+    /// 移除某一帧某个事件
+    /// </summary>
+    /// <param name="clipName"></param>
+    /// <param name="frame"></param>
+    public void ClearSpecialEvents(string clipName, int frame)
+    {       
+        //Animation ani = GetComponent<Animation>();
+        AnimationClip clip = GetClip(clipName);
+        Debug.Log("事件个数： " + clip.events.Length);
+        float time = frame /clip.frameRate;
+        for (int i = 0; i < clip.events.Length; i++)
+        {
+            if (clip.events[i].time == time)
+            {
+                ArrayList arr = new ArrayList(clip.events);
+                arr.RemoveAt(i);
+                clip.events=(AnimationEvent[])arr.ToArray(typeof(AnimationEvent));
+                Debug.Log("事件个数(移除后)： " + clip.events.Length);
+                break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 移除某一个动画的所有帧事件
+    /// </summary>
+    /// <param name="clipName"></param>
+    /// <param name="frame"></param>
+    public void ClearAllEvents(string clipName)
+    {        
+        AnimationClip[] acp= ani.runtimeAnimatorController.animationClips;
+        for (int i = 0; i < acp.Length; i++)
+        {
+            for (int j = 0; j < acp[i].events.Length; j++)
+            {
+                acp[i].events.SetValue(null, i);
+            }
+            acp[i].events = null;
+        }    
+    }
+
+
+    /// <summary>
+    /// 停止播放
+    /// </summary>
+    /// <param name="clipName"></param>
+    public void OnPause()
+    {
+        //Animation ani = GetComponent<Animation>();            
+       ani.speed = 0;
+    }
+
+    /// <summary>
+    /// /继续播放
+    /// </summary>
+    /// <param name="clipName"></param>
+    public void OnContinue()
+    {
+        ani.speed = 1;
     }
 }
