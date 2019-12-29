5a23f21c6a6c85a3aa7f8b5744f42cfce893f9d8
3094997376@qq.com <3094997376@qq.com>
2019-12-29 16:18:16 +0800

    添加模型位置归零脚本

---
+++ Common/Model/TestAABB.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TestAABB  {
+
+    [MenuItem("MyMenu/Do Test")]
+    static void DoTest()
+    {
+        Transform parent = Selection.activeGameObject.transform;
+        Vector3 postion = parent.position;
+        Quaternion rotation = parent.rotation;
+        Vector3 scale = parent.localScale;
+        parent.position = Vector3.zero;
+        parent.rotation = Quaternion.Euler(Vector3.zero);
+        parent.localScale = Vector3.one;
+
+
+        Vector3 center = Vector3.zero;
+        Renderer[] renders = parent.GetComponentsInChildren<Renderer>();
+        foreach (Renderer child in renders)
+        {
+            center += child.bounds.center;
+        }
+        center /= parent.GetComponentsInChildren<Transform>().Length;
+        Bounds bounds = new Bounds(center, Vector3.zero);
+        foreach (Renderer child in renders)
+        {
+            bounds.Encapsulate(child.bounds);
+        }
+
+        parent.position = postion;
+        parent.rotation = rotation;
+        parent.localScale = scale;
+
+        foreach (Transform t in parent)
+        {
+            t.position = t.position - bounds.center;
+        }
+        parent.transform.position = bounds.center + parent.position;
+
+    }
+}
