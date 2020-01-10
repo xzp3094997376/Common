c23f3417af6102970ee27dbbc84f1cedddbbea28
3094997376@qq.com <3094997376@qq.com>
2020-01-10 15:28:18 +0800

    更新数据资源

---
+++ Common/Data/TestScriptableAssets.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TestScriptableAssets", order = 1)]
+public class TestScriptableAssets : ScriptableObject {
+    public string prefabName;
+    public int numberOfPrefabsToCreate;
+    public Vector3[] spawnPoints;
+    public string[] desStrs;
+}
