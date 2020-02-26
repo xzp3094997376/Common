85cc726ce5c11d379d62a0c188ddbcd79ab83d0f
3094997376@qq.com <3094997376@qq.com>
2020-02-26 10:56:25 +0800

    编辑器文件更新

---
+++ Common/Editor/CompilingFinishedCallback.cs
@@ -0,0 +1,22 @@
+﻿
+// CompilingFinishedCallback.cs
+using GCSeries;
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public static class CompilingFinishedCallback
+{
+
+    [UnityEditor.Callbacks.DidReloadScripts]
+    private static void OnScriptsReloaded()
+    {
+        MRSystem mRSystem = GameObject.FindObjectOfType<MRSystem>();
+        mRSystem.isAutoSlant = false;
+        mRSystem.ViewerScale = 5;
+        //Debug.LogError("fsdfsf");
+        //Debug.Log("fd");
+    }
+}
---
+++ Common/Editor/PrefabUtils.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+public class PrefabUtils : Editor
+{
+    /// <summary>
+    /// 创建图片预制体
+    /// </summary>
+    [MenuItem("Assets/AssetsCreate/ImageToSpriteprefab", false, 1)]
+    static void CreateSpriteUGUIPrefab()
+    {
+        string[] guid = Selection.assetGUIDs;
+        string[] path = new string[] { AssetDatabase.GUIDToAssetPath(guid[0]) };
+        Debug.Log(path[0]);
+        string[] assets = AssetDatabase.FindAssets("t:texture2D", path);
+
+        GameObject go = new GameObject("SpritePrefab");
+        SpriteCtrl spCtrl = go.AddComponent<SpriteCtrl>();
+
+        for (int i = 0; i < assets.Length; i++)
+        {
+            string spritePath = AssetDatabase.GUIDToAssetPath(assets[i]);
+            Debug.Log(spritePath);
+            spCtrl.Add(AssetDatabase.LoadAssetAtPath<Sprite>(spritePath));
+        }
+
+        path[0] = Application.dataPath + "/Resources/Prefabs";
+        GameObject obj = PrefabUtility.CreatePrefab(path[0] + "/sprite.prefab", go);
+        DestroyImmediate(go);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
