aa29380f7aee3c71b17c19acf5ff8f9c87c6d6c3
allenxzp@outlook.com <allenxzp@outlook.com>
2020-10-23 14:59:56 +0800

    添加编辑器文件

---
+++ Common/Editor/OutputModelPath/OpenFileDialog.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OutputModelPath
+{
+    /// <summary>
+    /// https://blog.csdn.net/liang_704959721/article/details/43701575
+    /// </summary>
+    public class OpenFileDialog
+    {
+        [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        public static extern bool GetOpenFileName([In, Out] OpenFileDlg ofd);
+    }
+    public class SaveFileDialog
+    {
+        [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        public static extern bool GetSaveFileName([In, Out] SaveFileDlg ofd);
+    }
+
+    public class OpenFileFolder
+    {
+        [DllImport("shell32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr SHBrowseForFolder([In, Out] OpenDialogDir ofn);
+
+
+        [DllImport("shell32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        public static extern bool SHGetPathFromIDList([In] IntPtr pidl, [In, Out] char[] fileName);
+
+    }
+
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class FileDlg
+    {
+        public int structSize = 0;
+        public IntPtr dlgOwner = IntPtr.Zero;
+        public IntPtr instance = IntPtr.Zero;
+        public String filter = null;
+        public String customFilter = null;
+        public int maxCustFilter = 0;
+        public int filterIndex = 0;
+        public String file = null;
+        public int maxFile = 0;
+        public String fileTitle = null;
+        public int maxFileTitle = 0;
+        public String initialDir = null;
+        public String title = null;
+        public int flags = 0;
+        public short fileOffset = 0;
+        public short fileExtension = 0;
+
+        // 如果你保存的时候忘记加.txt时，
+        // 它会自动给你加上。
+        public String defExt = null;
+
+        public IntPtr custData = IntPtr.Zero;
+        public IntPtr hook = IntPtr.Zero;
+        public String templateName = null;
+        public IntPtr reservedPtr = IntPtr.Zero;
+        public int reservedInt = 0;
+        public int flagsEx = 0;
+    }
+
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class OpenDialogDir
+    {
+        public IntPtr hwndOwner = IntPtr.Zero;
+        public IntPtr pidlRoot = IntPtr.Zero;
+        public String pszDisplayName = null;
+        public String lpszTitle = null;
+        public UInt32 ulFlags = 0;
+        public IntPtr lpfn = IntPtr.Zero;
+        public IntPtr lParam = IntPtr.Zero;
+        public int iImage = 0;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class OpenFileDlg : FileDlg
+    {
+
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class SaveFileDlg : FileDlg
+    {
+
+    }
+}
