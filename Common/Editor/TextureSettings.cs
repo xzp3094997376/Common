66d78481d29d45687a9b5d5ba516d0fe90a0b008
谢展鹏 <3094997376@qq.com>
2021-06-03 14:43:15 +0800

    贴图大小自动设置

---
+++ Common/Editor/TextureSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class MyTexturePostprocessor : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        if (EditorUserBuildSettings.activeBuildTarget==BuildTarget.Android|| EditorUserBuildSettings.activeBuildTarget == BuildTarget.iOS)
+        {
+            //Debug.Log(assetPath +"dsds");        
+            if (assetPath.Contains("map"))
+            {
+                Debug.Log("模型贴图导入");
+                TextureImporter textureImporter = (TextureImporter)assetImporter;
+                if (textureImporter == null)
+                {
+                    Debug.Log("不是图片");
+                    return;
+                }
+                textureImporter.maxTextureSize = 512;
+            }
+        }
+      
+    }
+
+    
+}
